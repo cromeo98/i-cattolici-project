7edc86ee4f04b0cc64062e5e2ab73dfaedaaad67
cromeo98 <romeo.christian98@gmail.com>
2021-11-03 15:15:39 +0100

    add popo-up to delete btn

--- resources/views/admin/competition/index.blade.php
+++ resources/views/admin/competition/index.blade.php
@@ -52,18 +52,36 @@
                         <a href="{{route('admin.competition.edit', $item->id)}}" class="btn btn-warning">Modifica</a>
 
                         {{-- delete --}}
-                        <form action="{{route('admin.competition.destroy', $item->id)}}" method="POST" class="d-inline-block text-center">
+                        <form action="{{route('admin.competition.destroy', $item->id)}}" method="POST" class="d-inline-block text-center" id="form-destroy-{{$item->name}}">
                             {{-- Per ogni form bisogna inserire il token altrimenti il cambiamento non viene accettato dal sistema --}}
                             @csrf
                             @method('DELETE')
 
-                            <button type="submit" class="btn btn-danger">Elimina</button>
+                            <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#staticBackdrop{{$item->id}}">Elimina</button>
                             
-                        </form>
+                        </form> 
 
                     </div>
                 </div>
             </div> 
+            <!-- Modal -->
+            <div class="modal fade" id="staticBackdrop{{$item->id}}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="staticBackdropLabel">Elimina {{$item->name}}</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <span>Sei sicuro di voler eliminare la competizione {{$item->name}}? Non potrai recuperare i dati eliminati.</span>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Annulla</button>
+                            <button type="submit" form="form-destroy-{{$item->name}}" class="btn btn-danger">Elimina</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         @endforeach
         
     </div>
