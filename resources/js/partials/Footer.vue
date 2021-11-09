1dcfcc9e6bdc9b79f3368705a1e5217c6f4621d7
cromeo98 <romeo.christian98@gmail.com>
2021-11-09 12:51:31 +0100

    first formats of footer and main

---
+++ resources/js/partials/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+    <footer>
+        
+    </footer>
+</template>
+
+<script>
+
+export default {
+    name: 'Footer'
+}
+
+</script>
+
+<style scoped lang="scss">
+
+    @import '../../sass/app.scss';
+
+    footer{
+        height: 100px;
+        background-color: $blue;
+    }
+
+
+</style>
--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -3,13 +3,32 @@
 
     <Header/>
     
-    <!-- <main>
+    <main>
 
-      <router-view></router-view>
+      <div class="container">
+        <nav class="text-center">
+          <div class="row justify-content-center pt-5">
+            <div class="col-2 pt-5">
+              <a href="#" class="ms_main-nav-links">La squadra</a>
+            </div>
+            <div class="col-2 pt-5">
+              <a href="#" class="ms_main-nav-links">News</a></div>
+            <div class="col-2 pt-5">
+              <a href="#" class="ms_main-nav-links">Risultati</a></div>
+            <div class="col-2 pt-5">
+              <a href="#" class="ms_main-nav-links">Sponsorships</a></div>
+            <div class="col-2 pt-5">
+              <a href="#" class="ms_main-nav-links">Competizioni</a></div>
+            <div class="col-2 pt-5">
+              <a href="#" class="ms_main-nav-links">Contattaci</a></div>
+          </div>
+        </nav>
+      </div>
+      <!-- <router-view></router-view> -->
 
-    </main>   -->
+    </main>  
 
-    <!-- <Footer/> -->
+    <Footer/>
 
   </div>
 
@@ -17,19 +36,39 @@
 
 <script>
   import Header from '../partials/Header';
-//   import Footer from '../partials/Footer';
+  import Footer from '../partials/Footer';
 
   export default {
     name: 'App',
     components: {
-      Header
-    //   Footer
+      Header,
+      Footer
     }
   }
 </script>
 
 <style lang="scss">
 
-  @import '../../sass/commons';
+  @import '../../sass/app';
+
+  main{
+    height: calc(100vh - 170px);
+    background-image: url('../../img/bg-home.png');
+    background-size: cover;
+    background-position: -7px;
+
+    .ms_main-nav-links{
+      background-color: $yellow;
+      opacity: 0.8;
+      color: rgb(33, 37, 41);
+      text-decoration: none;
+      padding: 10px 15px;
+      border-radius: 25px;
+          &:hover{
+            color: inherit;
+            opacity: 1;
+          }
+    }
+  }
 
 </style>
